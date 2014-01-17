4c64c365ac2ab5f0e04a84608f8882070fe1937d
Craig Tracey <craigtracey@gmail.com>
2014-01-16 23:03:11 -0500

    Adding openstack-omnibus attributes
    Developing an attribute framework for enabling and disabling omnibus
    services. Users can override the portions they would like to change.
    For the time being, this will support Ubuntu only.

---
+++ attributes/default.rb
@@ -0,0 +1,43 @@
+#
+# Cookbook Name:: openstack-omnibus
+# Attributes:: default
+#
+# Copyright 2014, Craig Tracey <craigtracey@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+default['openstack']['omnibus']['enabled_services'] = [
+  'identity'
+]
+
+default['openstack']['omnibus']['services'] = {
+  'identity' => {
+    'venv' => '/opt/openstack/keystone',
+    'config_dir' => '/etc/keystone',
+    'services' => {
+      'keystone' => {
+        'command' => 'bin/keystone-all',
+        'user' => 'keystone',
+      }
+    }
+  },
+}
+
+default['openstack']['omnibus']['clients'] = [
+  'keystoneclient',
+]
+
+# upstart
+default['openstack']['omnibus']['upstart_job_dir'] = '/etc/init/'
+default['openstack']['omnibus']['upstart_job_suffix'] = '.conf'
