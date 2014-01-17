90afc006e0933e423c5428fcad1e227e8605b073
Craig Tracey <craigtracey@gmail.com>
2014-01-16 23:05:15 -0500

    Add a recipe that sets upstream attributes
    This recipe should be included early in the node's runlist. Doing so
    it will perform a number of node attributes in order to change the
    functionality of the subsequent upstream cookbooks.

---
+++ recipes/node_attributes.rb
@@ -0,0 +1,28 @@
+#
+# Cookbook Name:: openstack-omnibus
+# Recipe:: node_attributes
+#
+# Copyright 2014, Craig Tracey <craigtracey@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+enabled_services = node['openstack']['omnibus']['enabled_services']
+
+# this is not ideal, but he stackforge cookbooks make this a reality
+if enabled_services.include? 'identity'
+  %w{keystone mysql_python postgresql_python memcache_python}.each do |type|
+    #print "HERE"
+    node.set['openstack']['identity']['platform']["#{type}_packages"] = []
+  end
+end
